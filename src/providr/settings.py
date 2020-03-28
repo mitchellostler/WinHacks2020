cba542cdc8cde73c3dc697d16e53ac9d198f0f9a
codingXllama <osamaalchalabi2@gmail.com>
2020-03-28 05:38:41 -0400

    fixed files

--- src/providr/settings.py
+++ src/providr/settings.py
@@ -59,15 +59,9 @@ ROOT_URLCONF = 'providr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-<<<<<<< HEAD
         'DIRS': [os.path.join(BASE_DIR, 'accounts/templates/accounts')],
-=======
-<<<<<<< HEAD
         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-=======
         'DIRS': [os.path.join(BASE_DIR, 'templates.html')],
->>>>>>> 4eba7d2546dfdb3979308ae010f60cd513a2daaa
->>>>>>> master
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
