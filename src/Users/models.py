754fa7e2a8be95df208364818e898e62ab2b2f11
Andres <afmunevarm@hotmail.com>
2020-03-29 03:44:07 -0400

    widgets in users.models deleted

--- src/Users/models.py
+++ src/Users/models.py
@@ -5,11 +5,11 @@ from django import forms
 class MyUser(models.Model):
     email     = models.EmailField(max_length=30, blank=True)
     username  = models.CharField(max_length=20, blank=True, unique=True)
-    password  = models.CharField(max_length=20, widget=forms.PasswordInput, blank=True)
+    password  = models.CharField(max_length=20, blank=True)
     fname     = models.CharField(max_length=20, blank=True)
     lname     = models.CharField(max_length=20, blank=True)
     address   = models.CharField(max_length=40, blank=True)
 
 class Login(models.Model):
     username  = models.CharField(max_length=20, blank=True, unique=True)
-    password  = models.CharField(max_length=20, widget=forms.PasswordInput, blank=True)
+    password  = models.CharField(max_length=20)
