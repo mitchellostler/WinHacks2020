9b0a6bc5f8a8c415d91464347af01908b1d2530d
Mitchell Ostler <mlostler@uwaterloo.ca>
2020-03-29 00:27:14 -0400

    update accounts

--- src/providr/urls.py
+++ src/providr/urls.py
@@ -17,11 +17,6 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 
 # importing functions from the component(mini-application views)
-from accounts.views import (
-    home_page,
-    products_page,
-)
-
 
 # importing the require functions from the project vierws
 # from .views import (
@@ -40,36 +35,13 @@ from accounts.views import (
 from providr_posts.views import (
     user_post_create_view
     )
-from .views import (
-    home_page,
-    login_page,
-    signup_page,
-    dashboard_page,
-    profile_page,
-    example_page,
-    contact_page
-)
+
 
 urlpatterns = [
-    # path('login/', login_page),
-    # path('signup/', signup_page),
-    # path('dashboard/create_post>', create_post_page),
-    # path('profile/<str:user_id>', profile_page),
-    # path('blog/', user_post_list_view),
-    # path('blog-new/', user_post_create_view),
-    # path('blog/<str:slug>/', user_post_detail_view),
-    # path('example/', example_page),
-    path('', home_page),
-    path('login/', login_page),
-    path('signup/', signup_page),
-    path('dashboard/<str:user_id>', dashboard_page),
-    path('profile/<str:user_id>', profile_page),
+
+    path('users/', include('Users.urls')),
     path('post-new/', user_post_create_view),
     path('post/', include('providr_posts.urls')),
-    path('example/', example_page),
-    path('contact/', contact_page),
-    path('admin/', admin.site.urls),
-    path('products/', products_page),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls'))
 
