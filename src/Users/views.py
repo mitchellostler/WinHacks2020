5c7419e0e9d4be0442d0279078bbef41291b8ce2
Mitchell Ostler <mlostler@uwaterloo.ca>
2020-03-29 00:40:04 -0400

    fixed

--- src/Users/views.py
+++ src/Users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .forms import SignUpForm
-
+from .forms import SignUpForm, LoginForm
+from .models import Login, MyUser
+from  django.http import QueryDict
+from django.http import HttpResponseRedirect
 
 
 def signup_view(request):
@@ -10,5 +12,28 @@ def signup_view(request):
             form.save()
     form = SignUpForm()
     return render(request, 'signup.html', {'form':form})
+
+def login_view(request):
+    validated = False
+    nameex = False
+    if request.method == 'POST':
+        inUser = request.POST.__getitem__('username')
+        inPass = request.POST.__getitem__('password')
+        form = Login(request.POST)
+        try:
+            user = MyUser.objects.get(username=inUser)
+            nameex = True
+        except:
+            print('NONE')
+        if nameex:
+            if user.password == inPass:
+                validated = True
+        if validated:
+            print('PASSED')
+            return HttpResponseRedirect('login')
+        else:
+            print('Failed')
+    form = LoginForm(request.POST)
+    return render(request, 'login.html', {'form': form})
 
 # Create your views here
